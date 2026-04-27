487fbbc31ff48333b87db4aba468b0da2e289776
balibabu <balibabuchauhan1@gmail.com>
2026-04-27 23:10:21 +0545

    code organized, removed duplicates

--- storage_engine/storageHandler.py
+++ storage_engine/storageHandler.py
@@ -8,61 +8,35 @@ from github_engine.fileHandler import FileHandler
 
 load_dotenv()
 MASTER_KEY = os.getenv('MASTER_KEY')
+FILE_SIZE_LIMIT = int(os.getenv('FILE_SIZE_LIMIT'))
 
 
 class StorageHandler:
 
     def upload(raw_file):
-        # 1. Create the File record
-        file_obj = File.objects.create(
-            title=raw_file.name,
-            size=raw_file.size,
-            content_type=raw_file.content_type
-        )
-
-        # 2. Setup local temp directory
+        file_obj = File.objects.create(title=raw_file.name, size=raw_file.size, content_type=raw_file.content_type)
         temp_dir = os.path.join(settings.MEDIA_ROOT, 'temp_chunks', str(file_obj.id))
         os.makedirs(temp_dir, exist_ok=True)
-
-        index = 0
-        chunk_size = 1024 * 1024 * 5 # 5MB
-        
+        index, chunk_size = 0, FILE_SIZE_LIMIT
         while True:
             chunk_data = raw_file.read(chunk_size)
-            if not chunk_data:
-                break
-            
-            # (Assuming you use the derive_chunk_key and encrypt_chunk functions here)
+            if not chunk_data: break
             key = derive_chunk_key(MASTER_KEY, str(file_obj.id), index)
             encrypted_data = encrypt_chunk(chunk_data, key)
-            
-            # 3. Save locally
             local_path = os.path.join(temp_dir, f"{index}.enc")
-            with open(local_path, 'wb') as f:
-                f.write(encrypted_data)
-            
-            # 4. Create the Chunk record 
-            chunk = Chunk.objects.create(
-                file=file_obj,
-                index=index,
-                local_path=local_path
-            )
-            
-            # 5. TRIGGER BACKGROUND TASK
+            with open(local_path, 'wb') as f: f.write(encrypted_data)
+            chunk = Chunk.objects.create(file=file_obj, index=index, local_path=local_path)
             upload_chunk_to_cloud.delay(chunk.id)
-            
             index += 1
-
         return file_obj.id
 
     def download(file_uid):
         file = get_object_or_404(File, id=file_uid)
-
         def file_iterator():
             chunks=file.chunks.all()
             for chunk in chunks:
                 key = derive_chunk_key(MASTER_KEY, str(file.id), chunk.index)
-                encrypted_data = FileHandler.download(chunk.id, chunk.folder)
+                encrypted_data = FileHandler.download(str(chunk.id), chunk.folder)
                 decrypted_data = decrypt_chunk(encrypted_data, key)
                 yield decrypted_data
 
