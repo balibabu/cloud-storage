9bbacac6f1d25bcbf0112b1edb3a60d264cd759d
balibabu <balibabuchauhan1@gmail.com>
2026-05-03 08:05:43 +0545

    added regex path

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('storage/', include('storage.urls')),
-    path('', include('common.urls'))
+    re_path(r'^.*$', include('common.urls'))
 ]
