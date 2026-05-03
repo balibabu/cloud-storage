a624701f6f443adda473153d2f78c053851b77c5
bali <bali@gmail.com>
2026-05-03 14:20:34 +0545

    duck dns auto ipv6 update

---
+++ duckdns.py
@@ -0,0 +1,32 @@
+import requests, os, socket
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Replace these with your actual values
+domain = os.getenv('DUCKDNS_DOMAIN')
+token = os.getenv('DUCKDNS_TOKEN')
+
+def get_ipv6():
+    s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    try:
+        s.connect(("2001:4860:4860::8888", 80))
+        return s.getsockname()[0]
+    finally:
+        s.close()
+        return ''
+
+ip_address = get_ipv6()
+
+if ip_address:
+    url = f"https://duckdns.org/update/{domain}/{token}/{ip_address}"
+else:
+    url = f"https://duckdns.org/update/{domain}/{token}"
+
+try:
+    print("Ip Addr: ", ip_address)
+    response = requests.get(url)
+    print("Status Code:", response.status_code)
+    print("Response Text:", response.text)
+except requests.exceptions.RequestException as e:
+    print("Error:", e)
