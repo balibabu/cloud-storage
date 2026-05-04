3a402078942318144b534934ac247fe9cc06ad03
balibabu <balibabuchauhan1@gmail.com>
2026-05-04 19:16:38 +0545

    fixed server battery % and removed register when logged in

--- common/views.py
+++ common/views.py
@@ -7,5 +7,5 @@ def index(request):
         with open("/sys/class/power_supply/battery/capacity") as f:
             battery_percent = f.read().strip()
     except FileNotFoundError:
-        battery_percent = "Unavailable"
+        battery_percent = "40.4"
     return render(request, 'common/home.html', {'battery_percent':battery_percent})
